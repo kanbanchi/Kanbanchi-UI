9ef073b9b151c7e6aa2a046eb76ff41037619de6
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-03-01 13:03:42 +0400

    remove index.js from components

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,8 @@
-import Button from './button';
-import ButtonsGroup from './buttons_group';
-import ButtonsSegmented from './buttons_segmented';
-import Icon from './icon';
-import Snackbar from './snackbar';
+import Button from './button/button.js';
+import ButtonsGroup from './buttons_group/buttons_group.js';
+import ButtonsSegmented from './buttons_segmented/buttons_segmented.js';
+import Icon from './icon/icon.js';
+import Snackbar from './snackbar/snackbar.js';
 export {
     Button,
     ButtonsGroup,
