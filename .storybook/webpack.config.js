5da559f801c484dbeebf3ad0b9705d6772529a85
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-03-04 13:18:32 +0400

    remove react-scripts, import assets scss in modules

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,43 +1,28 @@
-const path = require('path'),
-    imports = {
-        data: `
-            @import 'material-colors-map';
-            @import 'mixins';
-            @import 'variables';
-            @import 'helpers';`,
-        includePaths: [
-            path.resolve(__dirname, '../src/assets/sass/partials')
-        ]
-    };
+const path = require('path');
 
-
-const sassModuleRegex = /\.module\.(scss|sass)$/;
-
-module.exports = (storybookBaseConfig, configType) => {
-    const baseSassModuleRule = storybookBaseConfig.module.rules.find(item => item.test.toString() === sassModuleRegex.toString());
-    const sassModuleRuleIndex = storybookBaseConfig.module.rules.indexOf(baseSassModuleRule);
-    storybookBaseConfig.module.rules.splice(sassModuleRuleIndex, 1);
-    storybookBaseConfig.module.rules.push({
-        test: sassModuleRegex,
-        include: path.resolve(__dirname, '../src/ui'),
-        use: [{
-                loader: 'style-loader',
-            },
+module.exports = {
+    module: {
+        rules: [
             {
-                loader: 'css-loader',
-                options: {
-                    modules: true,
-                    sourceMap: true,
-                    localIdentName: '[local]',
-                    minimize: true
-                }
-            },
-            {
-                loader: 'sass-loader',
-                options: imports
+                test: /\.module\.(scss|sass)$/,
+                include: path.resolve(__dirname, '../src/ui'),
+                use: [{
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true,
+                            localIdentName: '[local]',
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
-    });
-    return storybookBaseConfig;
+    }    
 };
-
