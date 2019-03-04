d95792d037a58b557afc7b88215e5eacd612e451
sergeyBashinskiy <sergey.bashinskiy@mail.magicwebsolutions.co.uk>
2019-03-04 12:05:50 +0400

    index exporters

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,12 +1,5 @@
-import Button from './button/button.js';
-import ButtonsGroup from './buttons_group/buttons_group.js';
-import ButtonsSegmented from './buttons_segmented/buttons_segmented.js';
-import Icon from './icon/icon.js';
-import Snackbar from './snackbar/snackbar.js';
-export {
-    Button,
-    ButtonsGroup,
-    ButtonsSegmented,
-    Icon,
-    Snackbar
-};
+export {Button} from './button/button.js';
+export {ButtonsGroup} from './buttons_group/buttons_group.js';
+export {ButtonsSegmented} from './buttons_segmented/buttons_segmented.js';
+export {Icon} from './icon/icon.js';
+export {Snackbar} from './snackbar/snackbar.js';
