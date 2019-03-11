488e4671827573d84fc1a3dba8d1ca07b6aac752
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-03-11 12:59:42 +0400

    filter empty classNames

--- src/ui/utils.js
+++ src/ui/utils.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 
-const ClassNames = (...props) => {
-    return props.join(' ');
-};
+const ClassNames = (...props) => props.filter(i => !!i).join(' ');
 
 export {
     PropTypes,
