4a650f96d6ea0b9004817d8b6ed54e90044df0d4
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-05-14 11:27:16 +0400

    add ClassVariants

--- src/ui/utils.js
+++ src/ui/utils.js
@@ -2,7 +2,16 @@ import PropTypes from 'prop-types';
 
 const ClassNames = (...props) => props.filter(i => !!i).join(' ');
 
+const ClassVariants = ({variants, prefix}) => {
+    let ret = '';
+    if (variants.length) {
+        ret += prefix + variants.join(' ' + prefix);
+    }
+    return ret;
+};
+
 export {
     PropTypes,
-    ClassNames
+    ClassNames,
+    ClassVariants
 };
