cbabc16a9870c02e7020fa7ba9854f9d8ea38427
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-05-24 11:48:00 +0400

    ts ButtonsGroup

---
+++ src/ui/buttonsGroup/buttonsGroup.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { IButtonsGroupProps } from './types';
+import { ClassNames } from '../utils';
+import '../../../src/ui/buttonsGroup/buttonsGroup.module.scss';
+
+export const ButtonsGroup: React.SFC<
+    IButtonsGroupProps
+    & React.HTMLAttributes<HTMLDivElement>
+> = 
+    (props) => {
+        let {
+            children,
+            className,
+            size,
+            ...attributes
+        } = props;
+
+        className = ClassNames(
+            'kui-buttons_group',
+            (size) ? 'kui-buttons_group--size_' + size : null,
+            className
+        );
+
+        return (
+            <div
+                className={className}
+                {...attributes}
+            >
+                {children}
+            </div>
+        );
+    }
+
+ButtonsGroup.defaultProps = {
+    size: null
+}
+
+ButtonsGroup.displayName = 'ButtonsGroup';
