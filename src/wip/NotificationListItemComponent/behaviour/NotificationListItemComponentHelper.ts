8cb350fe18e6a7752b5c6ee8ea5e6b2272475422
Andrey Lazarev <andrey.lazarev@mail.magicwebsolutions.co.uk>
2019-05-30 18:11:17 +0400

    declare consts in alph. order

--- src/wip/NotificationListItemComponent/behaviour/NotificationListItemComponentHelper.ts
+++ src/wip/NotificationListItemComponent/behaviour/NotificationListItemComponentHelper.ts
@@ -1,8 +1,8 @@
-export const pluralize = (num: number, str: string) => {
-    return num === 1 ? str : str + 's'
-};
-
 export const capitalize = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const pluralize = (num: number, str: string) => {
+    return num === 1 ? str : str + 's'
 };
 
