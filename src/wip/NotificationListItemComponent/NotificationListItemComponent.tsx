2899c23260c7bef164c1b34965726f63ef4d88c4
Andrey Lazarev <andrey.lazarev@mail.magicwebsolutions.co.uk>
2019-05-31 12:28:07 +0400

    changes required after code-review

--- src/wip/NotificationListItemComponent/NotificationListItemComponent.tsx
+++ src/wip/NotificationListItemComponent/NotificationListItemComponent.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {INotificationListItemProps} from './types';
 import {capitalize, pluralize} from './behaviour/NotificationListItemComponentHelper';
-import {Icon, Input, Select, SelectList} from '../../ui';
+import {Button, Icon, Input, Select, SelectList} from '../../ui';
 import './_NotificationListItemComponent.scss';
 
 const units = ['MINUTE', 'HOUR', 'DAY'];
@@ -10,18 +10,18 @@ export const NotificationListItemComponent: React.SFC<INotificationListItemProps
     props => {
         const {value, onValueChange, onUnitsChange, onTrash} = props;
         return (
-            <li className='notification-list-item'>
-                <label className='notification-list-item__label'>Notify</label>
-                <span className='value-input--container'>
+            <li className="notification-list-item">
+                <label className="notification-list-item__label">Notify</label>
+                <span className="value-input--container">
                     <Input
-                        type='number'
+                        type="number"
                         value={value.toString()}
-                        className='value-input'
+                        className="value-input"
                         onChange={(e: any) => onValueChange(e.target.value)}
                     />
                 </span>
-                <span className='units-select--container'>
-                    <Select className='units-select'
+                <span className="units-select--container">
+                    <Select className="units-select"
                             active={0}
                             onChange={(e: any) => onUnitsChange(e.item.value)}>
                         <SelectList>
@@ -37,10 +37,12 @@ export const NotificationListItemComponent: React.SFC<INotificationListItemProps
                         </SelectList>
                     </Select>
                 </span>
-                <span className='trash-notification--icon'
-                      onClick={() => onTrash()}>
-                    <Icon xlink='trash' size={24} />
-                </span>
+                <Button
+                    variant="icon"
+                    className="trash-notification--icon"
+                    onClick={() => onTrash()}>
+                    <Icon xlink="trash" size={24} />
+                </Button>
             </li>
         );
     };
