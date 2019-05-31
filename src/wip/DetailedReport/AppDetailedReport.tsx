05bc918a09282ec4e85f0bef7e7c124347f20ffa
Kirill Penkin <kirill.penkin@mail.magicwebsolutions.co.uk>
2019-05-31 13:25:52 +0400

    ProductivityDetailedReport

--- src/wip/DetailedReport/AppDetailedReport.tsx
+++ src/wip/DetailedReport/AppDetailedReport.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
 import {Chart} from 'react-google-charts'
 import { IAppProductivityDetailedProps } from './types';
-import { reportSettingsInitial } from './constants';
+import { reportSettingsInitial, initialProductivityPrivateProps } from './constants';
+import { ProductivitySettingsTransformer } from './behavior/productivitySettingsTransformer';
 
 export const AppDetailedProductivityChart: React.SFC<IAppProductivityDetailedProps> =
 (props) => {
+    const productivitySettingsTransformer = new ProductivitySettingsTransformer(props.reportSettings, props.reportData as any);
+    productivitySettingsTransformer.calculateProductivityDetailedChartData();
+    const data = productivitySettingsTransformer.getChartData();
+
+    const productivityOptions = productivitySettingsTransformer.getProductivityDetailedOptions();
+
+    const resultProps = {
+        ...initialProductivityPrivateProps,
+        ...productivityOptions,
+        data,
+    }
+
     return (
-        <div className="productivity-detailed__facade">Productivity Detailed facade</div>
+        <div className="productivity-detailed__facade">
+            <Chart {...resultProps}/>
+        </div>
     );
 }
 
@@ -15,4 +30,3 @@ AppDetailedProductivityChart.defaultProps = {
     reportData: {},
     reportSettings: reportSettingsInitial,
 }
-
