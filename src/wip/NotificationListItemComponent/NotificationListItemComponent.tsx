d0f8b03a555c6b29d448ae71530e0210b7fc6a7f
Andrey Lazarev <andrey.lazarev@mail.magicwebsolutions.co.uk>
2019-05-31 13:17:53 +0400

    changes required after code-review

--- src/wip/NotificationListItemComponent/NotificationListItemComponent.tsx
+++ src/wip/NotificationListItemComponent/NotificationListItemComponent.tsx
@@ -5,26 +5,46 @@ import {Button, Icon, Input, Select, SelectList} from '../../ui';
 import './_NotificationListItemComponent.scss';
 
 const units = ['MINUTE', 'HOUR', 'DAY'];
+const defaultUnit = 'MINUTE';
+const defaultValue = 5;
 
 export const NotificationListItemComponent: React.SFC<INotificationListItemProps> =
     (props) => {
         const {value, onValueChange, onUnitsChange, onTrash} = props;
+        let currentValue = defaultValue;
+        let currentUnit = units.indexOf(defaultUnit);
+
+        const onUnitsSelectChange = (e: any) => {
+            if (e && e.item && e.item.value) {
+                const unit = e.item.value;
+                onUnitsChange(unit);
+                currentUnit = units.indexOf(unit);
+            }
+        };
+
+        const onValueInputChange = (e: any) => {
+            if (e && e.target && e.target.value) {
+                currentValue = e.target.value;
+                onValueChange(currentValue)
+            }
+        };
+
         return (
             <li className="notification-list-item">
                 <label className="notification-list-item__label">Notify</label>
                 <span className="value-input--container">
                     <Input
-                        type="number"
-                        value={value.toString()}
                         className="value-input"
-                        onChange={(e: any) => onValueChange(e.target.value)}
+                        type="number"
+                        value={currentValue.toString()}
+                        onChange={(e: any) => onValueInputChange(e)}
                     />
                 </span>
                 <span className="units-select--container">
                     <Select className="units-select"
                             variant="arrow"
-                            active={0}
-                            onChange={(e: any) => onUnitsChange(e.item.value)}>
+                            active={currentUnit}
+                            onChange={(e: any) => onUnitsSelectChange(e)}>
                         <SelectList className="units-select--list">
                             {
                                 units.map(unit => {
@@ -49,7 +69,6 @@ export const NotificationListItemComponent: React.SFC<INotificationListItemProps
     };
 
 NotificationListItemComponent.defaultProps = {
-    value: 5,
     onValueChange: value => {
         console.log(Number(value));
         return Number(value)
