a9262cdfc887cc61e682ac7f9d20f294844053be
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-06-03 16:59:50 +0400

    fix divider

--- src/ui/divider/types.ts
+++ src/ui/divider/types.ts
@@ -1,2 +1,9 @@
 export interface IDividerProps {
 }
+
+export type IConflictFreeHTMLAttributes<E> = React.HTMLAttributes<E>;
+
+export interface IDividerInheritedProps extends
+    IDividerProps,
+    IConflictFreeHTMLAttributes<HTMLElement>
+{}
