2b98777cd8fe0daa6644c8235e4401f1a279dfc1
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-06-05 13:05:15 +0400

    delete jopa

--- src/ui/select/select.tsx
+++ src/ui/select/select.tsx
@@ -193,7 +193,7 @@ React.forwardRef((props, ref) => {
             })
             .then(() => { // dont know why it works only in then
                 if (e.which === 27) { // esc
-                    setValueHook(initialValue + 'jopa'); // doesnt reset to initialValue without it
+                    setValueHook(initialValue + ' '); // doesnt reset to initialValue without it
                     setIsOpenedHook(false);
                     setActiveHook(active);
                     setValue(initialValue);
