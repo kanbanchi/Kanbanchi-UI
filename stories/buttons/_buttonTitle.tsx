55a9876be90a476af295bbffeb53376a1c45326f
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-06-05 09:41:21 +0400

    story ButtonTitle

---
+++ stories/buttons/_buttonTitle.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button, ButtonTitle, Icon } from '../../src/ui';
+
+const Story = () => {
+    return (
+        <div className="page">
+            <section>
+                <h2>ButtonTitle</h2>
+
+                <Button variant="secondary">
+                    <ButtonTitle>Title</ButtonTitle>
+                    <Icon xlink="deadline" style={{marginLeft: '4px'}}/>
+                </Button>
+
+            </section>
+        </div>
+    );
+};
+
+storiesOf('Buttons', module)
+    .add('ButtonTitle', () => <Story/>);
