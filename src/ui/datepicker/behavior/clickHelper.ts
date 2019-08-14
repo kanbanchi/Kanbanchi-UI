670983beee7673186b3879ddc9fc68df779fe636
Andrey Philippov <andrey.philippov@gmail.com>
2019-08-14 11:52:02 +0400

    Revert "datepicker closes on inner input click"

--- src/ui/datepicker/behavior/clickHelper.ts
+++
@@ -1,9 +0,0 @@
-const kuiInputClassName = 'kui-input__item';
-const kuiInputTag = 'INPUT';
-
-export const isInnerInput =
-(
-    target: HTMLElement
-) => {
-    return (target.tagName === kuiInputTag) && (target.classList.contains(kuiInputClassName));
-}
