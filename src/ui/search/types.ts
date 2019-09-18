cc60a5e69c66acf7461f3759e6d82d3631e04a60
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-09-18 17:14:45 +0400

    fix search types

--- src/ui/search/types.ts
+++ src/ui/search/types.ts
@@ -1,22 +1,5 @@
-import { IDropdownPublicProps } from './../dropdown/types';
-import { IInputPublicProps } from './../input/types';
-import { ISelectDispatchProps, ISelectOwnProps } from './../select/types';
-
-export interface ISearchProps extends
-    ISelectDispatchProps,
-    ISelectOwnProps,
-    IDropdownPublicProps,
-    IInputPublicProps
-{}
-
-export type IConflictFreeHTMLAttributes<E> =
-    Pick<React.InputHTMLAttributes<E>, Exclude<keyof React.InputHTMLAttributes<E>,
-        'color' |
-        'value' |
-        'onChange'
-    >>;
+import { ISelectInheritedProps } from './../select/types';
 
 export interface ISearchInheritedProps extends
-    ISearchProps,
-    IConflictFreeHTMLAttributes<HTMLInputElement>
+    ISelectInheritedProps
 {}
