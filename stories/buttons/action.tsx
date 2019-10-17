a35d27f8e28191810278717a10696d9af5886bc2
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2019-10-17 16:06:22 +0400

    add color white in button variant action

--- stories/buttons/action.tsx
+++ stories/buttons/action.tsx
@@ -12,6 +12,22 @@ const Story = () => {
                     <Button variant="action" disabled>Disabled</Button>
                 </ButtonsGroup>
             </section>
+
+            <section className="section-grey">
+                <h2>Actions</h2>
+                <ButtonsGroup>
+                    <Button variant="action">Action</Button>
+                    <Button variant="action" disabled>Disabled</Button>
+                </ButtonsGroup>
+            </section>
+
+            <section className="section-purple">
+                <h2>Color="white"</h2>
+                <ButtonsGroup size="large">
+                    <Button variant="action" color="white">Action</Button>
+                    <Button variant="action" color="white" disabled>Disabled</Button>
+                </ButtonsGroup>
+            </section>
         </div>
     );
 };
