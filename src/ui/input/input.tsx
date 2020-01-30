db61328d576a6d580a52508152d2fecf65b50abb
Alexey <alexey.fominykh@mail.magicwebsolutions.co.uk>
2020-01-30 15:51:35 +0400

    check if e

--- src/ui/input/input.tsx
+++ src/ui/input/input.tsx
@@ -72,7 +72,7 @@ React.forwardRef((props, ref) => {
      */
 
     attributes.onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-        e.persist();
+        if (e) e.persist();
         setTimeoutHook(setTimeout(() => {
             if (isFocusedHook) {
                 setIsFocusedHook(false);
@@ -82,7 +82,7 @@ React.forwardRef((props, ref) => {
     }
 
     attributes.onFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-        e.persist();
+        if (e) e.persist();
         clearTimeout(timeoutHook);
         setTimeoutHook(setTimeout(() => {
             if (!isFocusedHook) {
