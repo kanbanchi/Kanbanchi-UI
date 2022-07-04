9c7ddb2cab44c3fca76912eb65b18c8ff6684acf
AlexeyFominykh <alexey.fominykh@kanbanchi.com>
2022-07-04 13:45:05 +0400

    KNB-2407 add storybook
    npx sb init --builder webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,15 @@
-const custom = require('./webpack.config.js');
-
 module.exports = {
-  stories: [
-    '../stories/**/*.(js|tsx)'
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      module: {
-        rules: custom.module.rules,
-      },
-      resolve: {
-        ...config.resolve,
-        ...custom.resolve,
-      }
-    };
-  },
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
